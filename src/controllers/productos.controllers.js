39d3c19eeac54e82d3383125fb7c2ab4b0a56603
Emilse Arias <emilsearias@gmail.com>
2025-08-11 21:17:14 -0300

    agregamos las funciones vacias para leerProductos y crearProducto

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -2,6 +2,13 @@ export const test = (req, res)=>{
     res.status(200);
     res.send('Primera prueba desde el backend')
 }
+
+export const leerProductos =(req, res)=>{
+
+}
 // agregar funcion para crear Producto
+export const crearProducto = (req, res) => {
+    
+}
 
 // agregar funcion para editar Producto
--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { test } from "../controllers/productos.controllers.js";
+import { crearProducto, leerProductos, test } from "../controllers/productos.controllers.js";
 
 const router = Router();
 // get, post, put, delete
 router.route('/test').get(test)
+router.route('/').get(leerProductos).post(crearProducto)
 
 export default router;
