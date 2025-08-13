2b5e1ea44e881a15bb9541466cd08aa54fc31ce0
Emilse Arias <emilsearias@gmail.com>
2025-08-13 20:18:56 -0300

    podemos ver los datos que llegan en la solicitud para crear un producto

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -1,14 +1,20 @@
-export const test = (req, res)=>{
-    res.status(200);
-    res.send('Primera prueba desde el backend')
-}
+export const test = (req, res) => {
+  res.status(200);
+  res.send("Primera prueba desde el backend");
+};
 
-export const leerProductos =(req, res)=>{
-
-}
+export const leerProductos = (req, res) => {};
 // agregar funcion para crear Producto
-export const crearProducto = (req, res) => {
-    
-}
+export const crearProducto = async (req, res) => {
+  try {
+    //1- recibir el objeto que tengo que agregar a la BD
+    console.log(req.body)
+    //2- Validar los datos del objeto
+    //3- guardar el objeto en la base de datos
+    //4- enviar respuesta
+  } catch (error) {
+    console.error(error)
+  }
+};
 
 // agregar funcion para editar Producto
