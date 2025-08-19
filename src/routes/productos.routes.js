0b58105db5bc482eadf52711713644d983b2ba90
Emilse Arias <emilsearias@gmail.com>
2025-08-18 21:39:03 -0300

    agregamos la funcionalidad para editar productos por id

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { borrarProductoPorId, crearProducto, leerProductoPorId, leerProductos, test } from "../controllers/productos.controllers.js";
+import { borrarProductoPorId, crearProducto, editarProductoPorId, leerProductoPorId, leerProductos, test } from "../controllers/productos.controllers.js";
 
 const router = Router();
 // get, post, put, delete solicitud, request
 router.route('/test').get(test)
 router.route('/').get(leerProductos).post(crearProducto)
-router.route('/:id').get(leerProductoPorId).delete(borrarProductoPorId)
+router.route('/:id').get(leerProductoPorId).delete(borrarProductoPorId).put(editarProductoPorId)
 
 export default router;
