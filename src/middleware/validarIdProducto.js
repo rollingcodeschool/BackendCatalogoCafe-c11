688c150723724e03ff318a71967d92d28f4ee256
Emilse Arias <emilsearias@gmail.com>
2025-09-01 21:35:53 -0300

    copiamos el contenido de la validacion de producto en validaridProducto

---
+++ src/middleware/validarIdProducto.js
@@ -0,0 +1,60 @@
+import { body } from "express-validator";
+import resultadoValidacion from "./resultadoValidacion.js";
+import Producto from "../models/producto.js";
+
+const validacionProducto = [
+  body("nombreProducto")
+    .notEmpty()
+    .withMessage("El nombre del producto es obligatorio")
+    .isLength({ min: 2, max: 100 })
+    .withMessage("El nombre del producto debe tener entre 2 y 100 caracteres")
+    .custom(async (valor, {req})=>{
+      const productoExistente = await Producto.findOne({nombreProducto: valor})
+      //no existe ningun producto con el nombre 'valor'
+      if(!productoExistente) return true
+      // verificar si es un PUT, chequear si el id del productoExistente es el mismo que el producto que estoy editando
+      if(req.params?.id && productoExistente._id.toString() === req.params.id) return true
+     
+      throw new Error('Ya existe un producto con ese nombre')
+    })
+    ,
+  body("precio")
+    .notEmpty()
+    .withMessage("El precio es un dato obligatorio")
+    .isNumeric()
+    .withMessage("El precio debe ser un número")
+    .custom((valor) => {
+      if (valor >= 50 && valor <= 1000000) {
+        return true;
+      } else {
+        throw new Error("El precio debe estar entre 50 y 1000000");
+      }
+    }),
+  body("imagen")
+    .notEmpty()
+    .withMessage("La imagen es un dato obligatorio")
+    .matches(/(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|jpeg|gif|png|webp)/)
+    .withMessage(
+      "La imagen debe ser una URL válida y debe terminar en .jpg, .jpeg, .png o .webp"
+    ),
+  body("categoria")
+    .notEmpty()
+    .withMessage("La categoria es un dato obligatorio")
+    .isIn(["Infusiones", "Batidos", "Dulce", "Salado"])
+    .withMessage(
+      "La categoria debe ser una de las siguientes opciones: Infusiones, Batidos, Dulce, Salado"
+    ),
+  body("descripcion_breve")
+    .notEmpty()
+    .withMessage("La descripcion breve es obligatoria")
+    .isLength({ min: 5, max: 250 })
+    .withMessage("La descripcion breve debe tener entre 5 y 250 caracteres"),
+  body("descripcion_amplia")
+    .notEmpty()
+    .withMessage("La descripcion amplia es obligatoria")
+    .isLength({ min: 10, max: 500 })
+    .withMessage("La descripcion amplia debe tener entre 10 y 500 caracteres"),
+  (req, res, next) => resultadoValidacion(req, res, next),
+];
+
+export default validacionProducto;
