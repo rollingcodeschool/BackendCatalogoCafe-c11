5c18460a3ba4a9301761eee080b451a3bf6db847
Emilse Arias <emilsearias@gmail.com>
2025-09-08 20:15:55 -0300

    agregamos la funcion de login para verificar el email y password

--- src/controllers/usuarios.controllers.js
+++ src/controllers/usuarios.controllers.js
@@ -1,7 +1,6 @@
 import Usuario from "../models/usuario.js";
 import bcrypt from "bcrypt";
 
-
 export const leerUsuarios = async (req, res) => {
   try {
     const listaUsuarios = await Usuario.find();
@@ -14,13 +13,17 @@ export const leerUsuarios = async (req, res) => {
 
 export const crearUsuario = async (req, res) => {
   try {
-    const {nombreUsuario, email, password} = req.body;
+    const { nombreUsuario, email, password } = req.body;
     //hashear el password
-    const saltos = bcrypt.genSaltSync(10)
-    console.log(saltos)
-    const passwordHash = bcrypt.hashSync(password, saltos)
-    console.log(passwordHash)
-    const nuevoUsuario = new Usuario({nombreUsuario, email, password: passwordHash});
+    const saltos = bcrypt.genSaltSync(10);
+    console.log(saltos);
+    const passwordHash = bcrypt.hashSync(password, saltos);
+    console.log(passwordHash);
+    const nuevoUsuario = new Usuario({
+      nombreUsuario,
+      email,
+      password: passwordHash,
+    });
     await nuevoUsuario.save();
     //4- enviar respuesta
     res.status(201).json({
@@ -29,5 +32,27 @@ export const crearUsuario = async (req, res) => {
   } catch (error) {
     console.error(error);
     res.status(500).json({ mensaje: "Error al crear el usuario" });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    //1- verificar si el email existe, si no existe enviar mensaje de error
+    const { email, password } = req.body;
+    const usuarioExistente = await Usuario.findOne({ email });
+    if (!usuarioExistente) {
+      return res.status(404).json({ mensaje: "No se encontro el usuario" });
+    }
+    //2- si existe el email, verificar el password. Si el password no es el mismo entonces enviar mensaje de error
+    const passwordVerificado = bcrypt.compareSync(password, usuarioExistente.password);
+    if(!passwordVerificado){
+      return res.status(401).json({mensaje: 'Credenciales incorrectas'})
+    }
+    //3- generar el token
+    //4- enviar la respuesta al frontend
+    res.status(200).json({ mensaje: "Login exitoso", nombreUsuario: usuarioExistente.nombreUsuario });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ mensaje: "Error al loguear el usuario" });
   }
 };
--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -2,9 +2,11 @@ import { Router } from "express";
 import {
   leerUsuarios,
   crearUsuario,
+  login,
 } from "../controllers/usuarios.controllers.js";
 import validacionUsuario from "../middleware/validarUsuario.js";
 
 const router = Router();
 router.route("/").get(leerUsuarios).post(validacionUsuario, crearUsuario);
+router.route('/login').post(login)
 export default router;
